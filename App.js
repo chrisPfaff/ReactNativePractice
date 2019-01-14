e610d0f1832a1337d1a4f985cbae0b4294a729ad
Chris Pfaff <chrispfaff10@gmail.com>
2019-01-14 15:25:37 -0600

    added a new component to App

--- App.js
+++ App.js
@@ -1,25 +1,34 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import Post from "./Post";
 
 export default class App extends React.Component {
   render() {
+    return <AppContainer />;
+  }
+}
+
+class Home extends React.Component {
+  render() {
     return (
       <View style={styles.container}>
-        <View>
-          <Text style={styles.red}>Hello</Text>
-        </View>
+        <Text>Home</Text>
       </View>
     );
   }
 }
+const Nav = createStackNavigator({
+  Home: { screen: Home },
+  Post: { screen: Post }
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center"
-  },
-  red: {
-    color: "red"
   }
 });
+
+const AppContainer = createAppContainer(Nav);
